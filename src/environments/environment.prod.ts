bc16839a596295adffacf1a1068e8f351abe1d23
paulohdelia <paulohdelia@gmail.com>
2021-03-31 22:39:04 -0300

    Configuração inicial firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCXi_fbNhSgKYRuQHkjYvo6_c5iPErN6qA",
+    authDomain: "ionic-music-player.firebaseapp.com",
+    projectId: "ionic-music-player",
+    storageBucket: "ionic-music-player.appspot.com",
+    messagingSenderId: "1001559176496",
+    appId: "1:1001559176496:web:733e96e2e57a6f51b744f5"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCXi_fbNhSgKYRuQHkjYvo6_c5iPErN6qA",
+    authDomain: "ionic-music-player.firebaseapp.com",
+    projectId: "ionic-music-player",
+    storageBucket: "ionic-music-player.appspot.com",
+    messagingSenderId: "1001559176496",
+    appId: "1:1001559176496:web:733e96e2e57a6f51b744f5"
+  },
 };
 
 /*
