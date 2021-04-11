2f73359178cfdff8211593a3bf62a9c4412fc3c5
paulohdelia <paulohdelia@gmail.com>
2021-04-11 18:21:46 -0300

    scroll infinito na página explorar

--- src/app/home/explorar/explorar.page.ts
+++ src/app/home/explorar/explorar.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+import { fakeSongList } from '@fake/songList';
 
 @Component({
   selector: 'app-explorar',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExplorarPage implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  ngOnInit() {
+  data: Array<{
+    id: string,
+    title: string,
+    author: string,
+    image: string,
+  }>;
+
+  isFinished = false;
+
+  constructor() {
   }
 
+  ngOnInit() {
+    this.getInitialData();
+  }
+
+  getInitialData() {
+    const initialData = fakeSongList.splice(0, 5);
+
+    this.data = initialData;
+  }
+
+  loadData(event) {
+    // Isso terá que ser adaptado quando estivermos usando o firebase...
+    const lastSongIndex = fakeSongList.indexOf(this.data[this.data.length - 1]);
+    const newData = fakeSongList.splice(lastSongIndex + 1, 3);
+    console.log(newData)
+
+    // Load fake para mostrar que está carregando os dados novos
+    // Puxando de um banco de dados real não tem necessidade disso
+    setTimeout(() => {
+      event.target.complete();
+
+      if (newData.length == 0) {
+        event.target.disabled = true;
+        this.isFinished = true;
+      }
+
+      this.data = [...this.data, ...newData];
+    }, 500);
+
+
+  }
 }
