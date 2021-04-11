ac0b04e08ef376738004f304924be74c2fa5392d
paulohdelia <paulohdelia@gmail.com>
2021-04-11 18:21:26 -0300

    dados fake para testes e implementação inicial

---
+++ fake/songList.ts
@@ -0,0 +1,56 @@
+export const fakeSongList = [
+  {
+    id: '0',
+    title: "Falling Asleep at 3:37 AM",
+    author: "Idealism",
+    image: "https://images.pexels.com/photos/2048865/pexels-photo-2048865.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    id: '1',
+    title: "Flamingosis",
+    author: "Football Head",
+    image: "https://images.pexels.com/photos/1119972/pexels-photo-1119972.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: '2',
+    title: "Flowers",
+    author: "The Deli",
+    image: "https://images.pexels.com/photos/1071188/pexels-photo-1071188.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  },
+  {
+    id: '3',
+    title: "Starving",
+    author: "Hailee Steinfeld, Grey",
+    image: "https://images.pexels.com/photos/112352/pexels-photo-112352.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: '4',
+    title: "Afterlife",
+    author: "Avenged Sevenfold",
+    image: "https://images.pexels.com/photos/5540995/pexels-photo-5540995.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: '5',
+    title: "Random Song",
+    author: "Random Guy",
+    image: "https://images.pexels.com/photos/6393140/pexels-photo-6393140.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: '6',
+    title: "Another Random Song",
+    author: "Random Girl",
+    image: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    id: '7',
+    title: "Random Art",
+    author: "Random Artist",
+    image: "https://images.pexels.com/photos/2156881/pexels-photo-2156881.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: '8',
+    title: "Beautiful Sky",
+    author: "Ireland",
+    image: "https://images.pexels.com/photos/66997/pexels-photo-66997.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+];
