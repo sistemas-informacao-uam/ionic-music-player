e835d227a358471fa94d7073a5bb7568bda302ff
guilherme <guilherme.gl1997@gmail.com>
2021-04-13 12:40:51 -0300

    nova pagina da playlist selecionada

--- src/app/home/playlist/playlist-routing.module.ts
+++ src/app/home/playlist/playlist-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { BibliotecaPage } from './biblioteca.page';
+import { PlaylistPage } from './playlist.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: BibliotecaPage,
-  },
+    component: PlaylistPage
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class BibliotecaPageRoutingModule {}
+export class PlaylistPageRoutingModule {}
